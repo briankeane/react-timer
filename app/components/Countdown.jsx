e3d9cc04d0f8ac41e08fd8968ba9f126679e2f42
briankeane <lonesomewhistle@gmail.com>
2017-04-01 14:52:27 -0500

    add countdown functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,12 +1,44 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      })
+    }, 1000);
+  },
   render: function () {
+    var {count} = this.state;
     return (
             <div>
-              <Clock totalSeconds={129}/>
+              <Clock totalSeconds={count}/>
+              <CountdownForm onSetCountdown={this.handleSetCountdown}/>
             </div>
       );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,13 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      Main:         'app/components/Main.jsx',
-      Nav:          'app/components/Nav.jsx',
-      Countdown:    'app/components/Countdown.jsx',
-      Timer:        'app/components/Timer.jsx',
-      Clock:        'app/components/Clock.jsx',
-      applicationStyles: 'app/styles/app.scss'
+      Main:               'app/components/Main.jsx',
+      Nav:                'app/components/Nav.jsx',
+      Countdown:          'app/components/Countdown.jsx',
+      CountdownForm:      'app/components/CountdownForm.jsx',
+      Timer:              'app/components/Timer.jsx',
+      Clock:              'app/components/Clock.jsx',
+      applicationStyles:  'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
   },
